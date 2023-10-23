ca46d3b8166a989b4f789cde50aca6ee7fa86a40
Charlie Aular <charlie@goodmeal.app>
2023-10-23 04:20:23 +0000

    adding exists conditions if exists model or sku

--- internal/infrastructure/repositories/products_repository.go
+++ internal/infrastructure/repositories/products_repository.go
@@ -1,6 +1,9 @@
 package repositories
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/charlieaular/products-go/internal/domain/contracts"
 	"github.com/charlieaular/products-go/internal/domain/models"
 	"gorm.io/gorm"
@@ -33,16 +36,44 @@ func (r ProductsRepository) DeleteById(id string) error {
 }
 
 func (r ProductsRepository) CreateProduct(model models.Product) (models.Product, error) {
+
+	var exists models.Product
+
+	err := r.DB.Model(models.Product{}).Where("lower(sku) = ?", strings.ToLower(model.SKU)).First(&exists).Error
+
+	if err != nil {
+		return models.Product{}, err
+	}
+
+	if (exists != models.Product{}) {
+		return models.Product{}, fmt.Errorf("product with sku %v already exists", model.SKU)
+	}
+
 	result := r.DB.Model(models.Product{}).Create(&model)
 
 	if result.Error != nil {
-		return models.Product{}, nil
+		return models.Product{}, result.Error
 	}
 
 	return model, nil
 }
 
 func (r ProductsRepository) UpdateProduct(id string, product models.Product) (models.Product, error) {
+	var exists models.Product
+
+	err := r.DB.Model(models.Product{}).
+		Where("lower(sku) = ?", strings.ToLower(product.SKU)).
+		Where("id != ?", id).
+		First(&exists).Error
+
+	if err != nil {
+		return models.Product{}, err
+	}
+
+	if (exists != models.Product{}) {
+		return models.Product{}, fmt.Errorf("product with sku %v already exists", product.SKU)
+	}
+
 	result := r.DB.Model(models.Product{}).Where("id = ?", id).Updates(&product)
 
 	if result.Error != nil {
